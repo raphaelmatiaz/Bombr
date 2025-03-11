8390f35a4455f86d805a6562275daec5f9b575f1
Rafael Matias <rafamatias@gmail.com>
2025-03-11 13:33:04 +0000

    Add PrismaORM + XataDB setup and update registration form validation

--- bombr/src/app/(auth)/register/page.tsx
+++ bombr/src/app/(auth)/register/page.tsx
@@ -17,7 +17,7 @@ function Login() {
                         <input className={styles.input} type="email" placeholder='Email' />
                         <input className={styles.input} type="text" placeholder='Full name' />
                     </span>
-                    <input className={styles.input} type="email" placeholder='Username' />
+                    <input className={styles.input} type="text" placeholder='Username' maxLength={15} pattern="[A-Za-z0-9._]+" title="Username can only contain letters, numbers, periods, and underscores" />
                     <input className={styles.input} type="password" placeholder='Password' />
                     <a className={`${styles.forgotPassword} ${styles.anchor}`} href="">forgot password?</a>
                     <button className={styles.loginButton}>Register</button>
