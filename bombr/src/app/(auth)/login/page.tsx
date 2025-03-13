7e52d2c8205da887a195a5ef807f0d233faa4dac
Rafael Matias <rafamatias@gmail.com>
2025-03-13 21:20:56 +0000

    Add ClickSpark animation and Particles background to login and register pages; include jsrepo configuration and update package.json for ogl dependency

--- bombr/src/app/(auth)/login/page.tsx
+++ bombr/src/app/(auth)/login/page.tsx
@@ -1,32 +1,59 @@
+"use client"
 import LoginProviders from '../../../components/LoginProviders/LoginProviders.tsx'
 import Logo from '../../../components/Logo/Logo.tsx'
 import Link from 'next/link'
 import styles from './login.module.css'
 import Dashboard from '../../../components/Auth/Dashboard.tsx'
-
+import ClickSpark from '../../../blocks/Animations/ClickSpark/ClickSpark.tsx'
+import SplashCursor from '../../../blocks/Animations/SplashCursor/SplashCursor.tsx'
+import Particles from '../../../blocks/Backgrounds/Particles/Particles.tsx';
 function Login() {
 
     return(
-        <div className={styles.loginWrapper}>
-            {/* <Dashboard></Dashboard> */}
+        <ClickSpark
+                sparkColor='#7A00F4'
+                sparkSize={10}
+                sparkRadius={35}
+                sparkCount={8}
+                duration={400}
+                >
+            <div className={styles.loginWrapper}>
+                {/* <Dashboard></Dashboard> */}
+                
+                    {/* Your content here */}
+                    
 
-            <aside className={styles.asideArt}  style={{backgroundImage: "./login.png"}}></aside>
-            
-            <main className={styles.formWrapper}>
-                <div className={styles.logo}></div>
-                <form className={styles.form} action="">
-                    <input className={styles.input} type="email" placeholder='Phone number, username or email' />
-                    <input className={styles.input} type="password" placeholder='Password' />
-                    <a className={`${styles.forgotPassword} ${styles.anchor}`} href="">forgot password?</a>
-                    <button className={styles.loginButton}>Login</button>
-                </form>
-                <p className={styles.or}>Or</p>
-                <hr className={styles.horizontalRow} />
-                <LoginProviders></LoginProviders>
-                <hr className={styles.horizontalRow} />
-                <p className={styles.p}>Don't have an account? <Link className={styles.anchor} href="/register">Sign up</Link></p>
-            </main>
-        </div>
+                <aside className={styles.asideArt}  style={{backgroundImage: "./login.png"}}>
+                    <Particles
+                        particleColors={['#7A00F4', '#FE2C3A', '#F31C96', '#C3B1E8', '#2B1B44']}
+                        particleCount={200}
+                        particleSpread={13}
+                        speed={0.1}
+                        particleBaseSize={100}
+                        moveParticlesOnHover={false}
+                        alphaParticles={false}
+                        disableRotation={false}
+                    />
+                    {/* <SplashCursor /> */}
+                </aside>
+                
+                <main className={styles.formWrapper}>
+                    <div className={styles.logo}></div>
+                    <form className={styles.form} action="">
+                        <input className={styles.input} type="email" placeholder='Phone number, username or email' />
+                        <input className={styles.input} type="password" placeholder='Password' />
+                        <a className={`${styles.forgotPassword} ${styles.anchor}`} href="">forgot password?</a>
+                        <button className={styles.loginButton}>Login</button>
+                    </form>
+                    <p className={styles.or}>Or</p>
+                    <hr className={styles.horizontalRow} />
+                    <LoginProviders></LoginProviders>
+                    <hr className={styles.horizontalRow} />
+                    <p className={styles.p}>Don't have an account? <Link className={styles.anchor} href="/register">Sign up</Link></p>
+                </main>
+            </div>
+
+        </ClickSpark>
     )
 }
 
--- bombr/src/app/(auth)/register/page.tsx
+++ bombr/src/app/(auth)/register/page.tsx
@@ -1,34 +1,55 @@
+"use client"
 import LoginProviders from '../../../components/LoginProviders/LoginProviders.tsx'
 import Logo from '../../../components/Logo/Logo.tsx'
 
 import styles from './register.module.css'
 import Link from 'next/link'
-
+import ClickSpark from '../../../blocks/Animations/ClickSpark/ClickSpark.tsx'
+import Particles from '../../../blocks/Backgrounds/Particles/Particles.tsx';
 function Login() {
     return(
-        <div className={styles.loginWrapper}>
+        <ClickSpark
+                sparkColor='#7A00F4'
+                sparkSize={10}
+                sparkRadius={35}
+                sparkCount={8}
+                duration={400}
+                >
+            <div className={styles.loginWrapper}>
 
-            <aside className={styles.asideArt}  style={{backgroundImage: "./login.png"}}></aside>
-            
-            <main className={styles.formWrapper}>
-                <div className={styles.logo}></div>
-                <form className={styles.form} action="">
-                    <span className={styles.inputWrapper}>
-                        <input className={styles.input} type="email" placeholder='Email' />
-                        <input className={styles.input} type="text" placeholder='Full name' />
-                    </span>
-                    <input className={styles.input} type="text" placeholder='Username' maxLength={15} pattern="[A-Za-z0-9._]+" title="Username can only contain letters, numbers, periods, and underscores" />
-                    <input className={styles.input} type="password" placeholder='Password' />
-                    <a className={`${styles.forgotPassword} ${styles.anchor}`} href="">forgot password?</a>
-                    <button className={styles.loginButton}>Register</button>
-                </form>
-                <p className={styles.or}>Or</p>
-                <hr className={styles.horizontalRow} />
-                <LoginProviders></LoginProviders>
-                <hr className={styles.horizontalRow} />
-                <p className={styles.p}>Already have an account? <Link className={styles.anchor} href="/login">Log in</Link></p>
-            </main>
-        </div>
+                <aside className={styles.asideArt}  style={{backgroundImage: "./login.png"}}>
+                    <Particles
+                            particleColors={['#6ba607', '#cc8108', '#448519', '#EE91C0', '#FCBB49', '#B260C4']}
+                            particleCount={150}
+                            particleSpread={10}
+                            speed={0.07}
+                            particleBaseSize={100}
+                            moveParticlesOnHover={false}
+                            alphaParticles={false}
+                            disableRotation={false}
+                        />
+                </aside>
+                
+                <main className={styles.formWrapper}>
+                    <div className={styles.logo}></div>
+                    <form className={styles.form} action="">
+                        <span className={styles.inputWrapper}>
+                            <input className={styles.input} type="email" placeholder='Email' />
+                            <input className={styles.input} type="text" placeholder='Full name' />
+                        </span>
+                        <input className={styles.input} type="text" placeholder='Username' maxLength={15} pattern="[A-Za-z0-9._]+" title="Username can only contain letters, numbers, periods, and underscores" />
+                        <input className={styles.input} type="password" placeholder='Password' />
+                        <a className={`${styles.forgotPassword} ${styles.anchor}`} href="">forgot password?</a>
+                        <button className={styles.loginButton}>Register</button>
+                    </form>
+                    <p className={styles.or}>Or</p>
+                    <hr className={styles.horizontalRow} />
+                    <LoginProviders></LoginProviders>
+                    <hr className={styles.horizontalRow} />
+                    <p className={styles.p}>Already have an account? <Link className={styles.anchor} href="/login">Log in</Link></p>
+                </main>
+            </div>
+        </ClickSpark>
     )
 }
 
