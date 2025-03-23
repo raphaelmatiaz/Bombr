b78743edf6a8c4e0dab49a612c9d6b71d4c1a77c
Rafael Matias <rafamatias@gmail.com>
2025-03-23 01:52:17 +0000

    Refactor authentication setup; add client ID and secret for Google and GitHub providers, and update sign-in callback logic

--- bombr/src/auth/authSetup.ts
+++ bombr/src/auth/authSetup.ts
@@ -5,6 +5,8 @@ import GitHub from "next-auth/providers/github";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
       authorization: {
         params: {
           prompt: "consent",
@@ -13,16 +15,45 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         },
       },
     }),
-    GitHub,
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
   ],
   pages: {
     signIn: "/login",
   },
   callbacks: {
-    authorized: async ({ auth }) => {
+    signIn: async ({ user, account, profile, email, credentials }) => {
       // Logged in users are authenticated, otherwise redirect to login page
-      return !!auth;
+      return true;
     },
   },
-  trustHost: true,
 });
+
+
+// Previous Code
+// export const { handlers, signIn, signOut, auth } = NextAuth({
+//   providers: [
+//     Google({
+//       authorization: {
+//         params: {
+//           prompt: "consent",
+//           access_type: "offline",
+//           response_type: "code",
+//         },
+//       },
+//     }),
+//     GitHub,
+//   ],
+//   pages: {
+//     signIn: "/login",
+//   },
+//   callbacks: {
+//     authorized: async ({ auth }) => {
+//       // Logged in users are authenticated, otherwise redirect to login page
+//       return !!auth;
+//     },
+//   },
+//   trustHost: true,
+// });
