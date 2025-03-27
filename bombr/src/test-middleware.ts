e82591f6aaf908896f40cdedca2bec0f407bac9b
Rafael Matias <rafamatias@gmail.com>
2025-03-27 17:51:33 +0000

    Remove test middleware file and associated commented code.

--- bombr/src/test-middleware.ts
+++
@@ -1,9 +0,0 @@
-// export { auth as middleware } from "@/auth/authSetup";
-
-// console.log("hello from middleware");
-
-// export const config = {
-//   // matcher: ["/((?!api|/|_next/static|_next/image|favicon.ico).*)"],
-//   matcher: ["/my-account"],
-
-// };
